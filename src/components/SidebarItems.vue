eb390a832a996d2bebc19ed6ba732e2e92dd5832
yrnThiago <thiagodias_123@hotmail.com>
2023-06-22 15:42:38 -0300

    fix: refactor

--- src/components/SidebarItems.vue
+++ src/components/SidebarItems.vue
@@ -85,33 +85,35 @@
 
 <template>
   <VBtn v-if="!isLogged" class="mt-6 mb-3" block color="background-black">ENTRAR</VBtn>
-  <VDivider></VDivider>
+  <VDivider class="pa-2"></VDivider>
 
   <v-list>
-    <v-list-item prepend-icon="mdi-magnify" title="Pesquisar" @click="redirectTo('/buscar')"></v-list-item>
+    <v-list-item class="pa-4" prepend-icon="mdi-magnify" title="Pesquisar" @click="redirectTo('/buscar')"></v-list-item>
   </v-list>
-  <VDivider></VDivider>
+  <VDivider class="pa-2"></VDivider>
 
   <v-list v-if="isLogged">
     <v-list-item prepend-icon="mdi-account-circle-outline" title="Usuário" @click="redirectTo('/market')"></v-list-item>
-    <VDivider></VDivider>
+    <VDivider class="pa-2"></VDivider>
     <v-list-item v-for="item in userItems" :key="item.value" :prepend-icon="item.icon" :title="item.title" @click="redirectTo(item.route)"></v-list-item>
-    <VDivider></VDivider>
+    <VDivider class="pa-2"></VDivider>
     <v-list-item prepend-icon="mdi-fingerprint" title="Documentos" @click="redirectTo('/documentos')"></v-list-item>
     <v-list-item prepend-icon="mdi-brightness-2" title="Modo escuro" @click="showPopUpDarkMode = true"></v-list-item>
   </v-list>
-  <VDivider></VDivider>
+  <VDivider class="pa-2"></VDivider>
 
   <v-list>
-    <v-list-item v-for="item in aboutItems" :key="item.value" :prepend-icon="item.icon" :title="item.title" @click="redirectTo(item.route)"></v-list-item>
+    <v-list-item v-for="item in aboutItems" :key="item.value" :prepend-icon="item.icon" @click="redirectTo(item.route)">
+      <p class="ml-2">{{ item.title }}</p>
+    </v-list-item>
   </v-list>
-  <VDivider></VDivider>
+  <VDivider class="pa-2"></VDivider>
 
   <v-list>
-    <v-list-item prepend-icon="mdi-brightness-2" title="Modo escuro" @click="showPopUpDarkMode = true"></v-list-item>
-    <v-list-item prepend-icon="mdi-cog" title="Configurações" @click="redirectTo('/configuracoes')"></v-list-item>
+    <v-list-item prepend-icon="mdi-brightness-2" @click="showPopUpDarkMode = true"><p class="ml-2">Modo escuro</p></v-list-item>
+    <v-list-item prepend-icon="mdi-cog"  @click="redirectTo('/configuracoes')"><p class="ml-2">Configurações</p></v-list-item>
   </v-list>
-  <VDivider></VDivider>
+  <VDivider class="pa-2"></VDivider>
 
   <div class="pt-10 pb-10"></div>
 
